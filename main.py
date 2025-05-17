5c748b204feb2450e1856a441fec5c4c77bf0860
artemcxvb <sakara.artem24@gmail.com>
2025-05-17 14:56:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def hello():
-    return "✅ Бот працює!"
+    return "🟢 Бот працює!"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    print("📨 Получен POST от Telegram")
+    data = request.get_json()
+    print(data)
+    return "ok"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
